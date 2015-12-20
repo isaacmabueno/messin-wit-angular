329cbf1e04d0bab089638ea73429eb00438d765f
Isaac Bueno <isaacmabueno@gmail.com>
2015-12-20 16:43:45 -0600

    fix app.js

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,14 @@ angular.module('todoListApp', [])
   $scope.test = function() {
     console.log('Test 1,2,3 on the mainCtrl');
   };
+})
+
+.controller('coolCtrl', function ($scope) {
+  $scope.editS = function() {
+    console.log("You are clicking the edit button");
+  };
+
+  $scope.test = function() {
+    console.log("this is not the main- instead this is the sub");
+  }
 });
