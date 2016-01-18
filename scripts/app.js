200598d6ea6f21889651e49ad7b4951db178323b
Isaac Bueno <isaacmabueno@gmail.com>
2016-01-18 13:38:40 -0600

    testing save button and adding in json(sort of)

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,29 @@
 angular.module('todoListApp', [])
-.controller('mainCtrl', function($scope) {
+.controller('mainCtrl', function($scope, dataService) {
+  $scope.helloConsole = dataService.helloConsole;
+
   $scope.changeNg = function() {
     console.log('something changed here in real time- test');
   };
-  $scope.todos = [
-  {"name": "mail thank you's"},
-  {"name": "cut out center pieces"},
-  {"name": "pay for the rest of the flowers"}
-]
+//   $scope.todos = [
+//   {"name": "mail thank you's"},
+//   {"name": "cut out center pieces"},
+//   {"name": "pay for the rest of the flowers"}
+// ]
 })
-
-.controller('coolCtrl', function ($scope) {
-  $scope.editS = function() {
-    console.log("You are clicking the edit button");
-  };
-
-  $scope.test = function() {
-    console.log("this is not the main- instead this is the sub");
-  };
-
+.service('dataService', function() {
+  this.helloConsole = function() {
+    console.log('this is the hello console service!');
+  }
 });
+
+// .controller('coolCtrl', function ($scope) {
+//   $scope.editS = function() {
+//     console.log("You are clicking the edit button");
+//   };
+
+  // $scope.test = function() {
+  //   console.log("this is not the main- instead this is the sub");
+  // };
+
+// });
