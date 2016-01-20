be79a1c99e3c6f3e67cc35941c71d2998457c949
Isaac Bueno <isaacmabueno@gmail.com>
2016-01-19 22:19:27 -0600

    delete button working in console no real REST api

--- scripts/app.js
+++ scripts/app.js
@@ -5,16 +5,34 @@ angular.module('todoListApp', [])
   $scope.changeNg = function() {
     console.log('something changed here in real time- test');
   };
+  $scope.todos = dataService.getTodos();
 //   $scope.todos = [
 //   {"name": "mail thank you's"},
 //   {"name": "cut out center pieces"},
 //   {"name": "pay for the rest of the flowers"}
 // ]
-})
-.service('dataService', function() {
+});
+
+
+
+.service('dataService', function($http) {
   this.helloConsole = function() {
     console.log('this is the hello console service!');
+  };
+  this.getTodos = $http.get('mock/todos.json').then(function(response){
+    console.log(resonse.data);
+    return response.data;
+  });
+  $scope.deleteTodo = function(dataService) {
+    dataService.deleteTodo(todo);
   }
+
+  this.deleteTodo = function(todo) {
+    console.log("The " + todo.name + " todo has been deleted!")
+  };
+  this.saveTodo = function(todo) {
+    console.log("The" + todo.name + " has been saved")
+  };
 });
 
 // .controller('coolCtrl', function ($scope) {
