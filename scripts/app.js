e28f9db0b570744eb64b63ada3b5f1536f19e964
Isaac Bueno <isaacmabueno@gmail.com>
2016-08-02 16:09:27 -0500

    adding all labels locally

--- scripts/app.js
+++ scripts/app.js
@@ -22,7 +22,7 @@ var beers = [
   ibu: 46,
   description: 'For all the bandwagon hop heads and fickle, uninformed craft beer dabblers... this beer is not about following the crowd or joining the masses of taste bud destroying, one and done beers…this is a different type of IPA; the unassuming, yet assertive type; the type that need not outdo another because this beer simply is what it is – upfront hoppy, floral character complimenting a grainy, classic English malt profile',
   canPurchase: true,
-  image: 'http://i.imgur.com/393E2vv.png?1',
+  image: 'images/ipa.png',
 },
 {
   name: 'English Mild',
@@ -31,6 +31,7 @@ var beers = [
   ibu: 20,
   description: 'A subtle, yet complex flavor profile. Up front rich maltiness dominates, but to the more distinguishing palette a slight grassy note from the Goldings hops emerges before the dry, crisp finish. A very easy to drink “session” beer enjoyed for centuries by the hard working English middle class.',
   canPurchase:true,
+  image: 'images/mild.png',
 
 
 },
@@ -41,6 +42,7 @@ var beers = [
   ibu: 30,
   description:'Inspired by the prototypical Fuller’s ESB. This ale exudes custom and tradition. Fermented in open stainless steel tanks, distinctive fruity esters and slight butterscotch undertones highlight the Englishness of this true English ale. Dryhopped with Goldings to create a very pleasing, full flavor that finishes with a refined and articulate character.',
   canPurchase: true,
+  image: 'images/esb.png',
 
 },
 {
@@ -50,6 +52,7 @@ var beers = [
   ibu: 24,
   description: 'Thick and creamy with pure, unadulterated roasty sweetness. Black as night, with very little bitterness coming through, finishing smooth as silk. A complex malt profile contributes recognizable fruitiness and caramel flavor to round out this dark and fulfilling stout.',
   canPurchase: true,
+  image: 'images/stout.png',
 },
 ];
 
@@ -61,6 +64,7 @@ var seasonals = [
   ibu: 23,
   description: 'A beer that has earned the distinction of carrying the name of one of history’s greatest brewing cities. This adaptation harkens back to the Ind Coope original - a pale ale which finds its identity in its water establishing a firm and crisp bitterness from an engimatic concoction of English hops. Smooth, crisp, and refreshing – an homage to the far distant forefather of today’s IPA.',
   canPurchase: true,
+  image: 'images/burton.png',
 },
 {
   name: 'Ordinary Bitter',
@@ -69,6 +73,7 @@ var seasonals = [
   ibu: 36,
   description: 'The bready, biscuity malt flavor establishes a distinct English character to balance a fruity and floral hop assertiveness. Medium bodied with a satisfying bitter finish.',
   canPurchase: true,
+  image: 'images/ordinary.png',
 
 },
 {
@@ -78,6 +83,7 @@ var seasonals = [
   ibu: 40,
   description: 'Bright and complex. Slightly nutty, toasty, and honey sweet accentuated with subtle yet complex fruity esters. Graham cracker malt profile allows a traditional hop character to flourish.',
   canPurchase: true,
+  image: 'images/premium.png',
 
 },
 {
@@ -87,6 +93,7 @@ var seasonals = [
   ibu: 28,
   description: 'This beer does not fit in a box; it transcends any label that you would put on it. Not quite a porter, not quite a brown ale. The deep, roasty character of a porter finds its way into a deep amber ale with subtle dried fruit qualities and an easy to drink finish.',
   canPurchase: true,
+  image: 'images/porter.png',
 
 },
 {
@@ -96,6 +103,7 @@ var seasonals = [
   ibu: 26,
   description: 'The quintessence of a refreshing libation. This crisp English golden ale is a perfect complement to a warm, sunny day. Malt forward with a distinct grainy character gradually finishing with an assertive spicy and floral hop note. Not too sweet, not too dry... perfect for another pint!',
   canPurchase: true,
+  image: 'images/summer.png',
 },
 {
   name: 'Summer Brown',
@@ -104,6 +112,7 @@ var seasonals = [
   ibu: 36,
   description: 'Fully steeped in English tradition with its eyes set on the land down under. This brown ale has all of the right chocolatey and roasty notes balanced by a key lime and tropical fruit hoppiness from Australian Galaxy hops.',
   canPurchase: true,
+  image: 'images/summerbrown.png'
 },
 ];
 
